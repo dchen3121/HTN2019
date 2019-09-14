cdeae9dc5aafec4ea3f25fa81893449a45782d47
Jason Sang <dotsangjason@gmail.com>
2019-09-14 11:37:42 -0400

    webcam implementation with backend

---
+++ webcam/app.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const express = require('express');
+const WebSocket = require('ws');
+const app = express();
+const request = require('request-promise');
+
+const WS_PORT = process.env.WS_PORT || 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+
+const wsServer = new WebSocket.Server({ port: WS_PORT }, () => console.log(`WS server is listening at ws://localhost:${WS_PORT}`));
+
+// array of connected websocket clients
+let connectedClients = [];
+
+wsServer.on('connection', (ws, req) => {
+    console.log('Connected');
+    // add new connected client
+    connectedClients.push(ws);
+    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
+    ws.on('message', data => {
+        // send the base64 encoded frame to each connected ws
+        let url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/0799ea6d-d91a-487f-9f89-c06de9cc1466/classify/iterations/Iteration3/image";
+            request(url, {
+                method: 'POST',
+                body: data,
+                headers: {
+                    "Prediction-Key": "60c283a92d554a958c43d0da935e9dfc",
+                    "Content-Type": "application/octet-stream"
+                }
+            })
+            .then((res) => {
+                return res
+            })
+            .then((res) => {
+                console.log(res);
+                connectedClients.forEach((ws, i) => {
+                    if (ws.readyState === ws.OPEN) { // check if it is still connected
+                        ws.send(res); // send
+                    } else { // if it's not connected remove from the array of connected ws
+                        connectedClients.splice(i, 1);
+                    }
+                });
+            })
+            .catch((e) => {
+                console.error(e);
+            })
+        // Don't need this block, we need to send data to openpose
+    });
+});
+
+app.use(express.static('public'))
+
+app.get('/client', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+
+
+app.listen(HTTP_PORT, () => {
+    console.log(`HTTP server listening at http://localhost:${HTTP_PORT}`)
+});
---
+++ webcam/public/index.js
@@ -0,0 +1,67 @@
+var video = document.querySelector("#videoElement");
+var canvas = document.createElement("canvas");
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video: { width: 426, height: 240 }})
+        .then(function (stream) {
+            video.srcObject = stream;
+        })
+        .catch((e) => {console.error(e)});
+
+    const WS_URL = 'ws://localhost:3001';
+    const ws = new WebSocket(WS_URL);
+    ws.onopen = () => {
+        console.log(`Connected to ${WS_URL}`);
+        setInterval(() => {
+            const canvas = document.createElement('canvas');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext('2d').drawImage(video, 0, 0);
+            canvas.toBlob((b) => {
+                ws.send(b);
+            })
+        }, 3000);
+    }
+
+    ws.onmessage = message => {
+        console.log(message);
+        const slouchRoot = document.querySelector('#slouch');
+        data = JSON.parse(message.data);
+        console.log(data);
+        slouchRoot.textContent = data.predictions[0].tagName;
+
+    }
+}
+
+// async function takePicture() {
+//     let context = canvas.getContext("2d");
+
+//     let width = 1280;
+//     let height = 720;
+
+//     context.drawImage(video, 0, 0, width, height);
+
+//     const data = canvas.toBlob(async (b) => {
+
+//             let url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/0799ea6d-d91a-487f-9f89-c06de9cc1466/classify/iterations/Iteration3/image";
+
+//             fetch(url, {
+//                 method: 'POST',
+//                 body: b,
+//                 headers: {
+//                     "Prediction-Key": "60c283a92d554a958c43d0da935e9dfc",
+//                     "Content-Type": "application/octet-stream"
+//                 }
+//             })
+//             .then((res) => {
+//                 return res.json();
+//             })
+//             .then((res) => {
+//                 console.log(res);
+//             })
+//             .catch((e) => {
+//                 console.error(e);
+//             })
+        
+//     })
+// }
