299e3c9f37dd4a959e1af7472cff911a970a07b2
dchen3121 <dchen001213@gmail.com>
2019-09-14 09:19:30 -0400

    added new endpoints, registered blueprints

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, redirect, url_for, session, render_template
-# from views.some_stuff import ...
+from views.users import user_blueprint
+from views.data import data_blueprint
 
 app = Flask(__name__)
 app.secret_key = "s7@8ob3fn8$%9nf64o&bv02q84gn74v!o78o34u78#27g4fno49lu*7h87q23fb1ui"
@@ -10,7 +11,10 @@ app.config.update(
 )
 # we might not need this, change later
 
-# app.register_blueprint(alert_blueprint, url_prefix="/...")  # do this later
+
+app.register_blueprint(user_blueprint, url_prefix="/users")
+app.register_blueprint(data_blueprint, url_prefix="/users")
+
 
 @app.route('/')
 def home():
