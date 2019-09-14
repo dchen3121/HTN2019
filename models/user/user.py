7630a3ecee641da348e556ca437bf17e7a79ddb1
pmielnik <patricia.mielnik1@gmail.com>
2019-09-14 06:05:04 -0400

    FCM basics

--- models/user/user.py
+++ models/user/user.py
@@ -6,6 +6,8 @@ from models.model import Model
 from common.utils import Utils
 import models.user.errors as errors
 
+from firebase_admin import auth #TODO: may move this functionality elsewhere
+# to initialize the app: default_app = firebase_admin.initialize_app()
 
 @dataclass(eq=False)
 class User(Model):
@@ -43,6 +45,39 @@ class User(Model):
         if not Utils.check_hashed_password(password, user.password):
             raise errors.IncorrectPasswordError('The password entered was incorrect.')
         return True
+
+    @classmethod
+    def update_slouch_data():
+    #TODO: send slouch notif command to FCM
+    # using the firebase admin sdk for python
+    # specs: https://firebase.google.com/docs/reference/admin/python/firebase_admin.messaging#webpushnotification
+    firebase_admin.messaging.WebpushNotification("Look out, you're slouching!", None, "ICON_URL")
+
+    #update times slouched list
+    #TODO: does mongo have same specifics (or can we just update and it'll create regardless?)
+    #TODO: remove the users.val(); still want to find+update data
+    if users.val() == None:
+        #SET initial data value for user
+        data = {'timesSlouched': [  #TODO: decide how we actually want to store data???
+                {'date' : Utils.get_date(), 'numSlouch' : 0},
+                {'date' : Utils.get_date(), 'numSlouch' : 0},
+                {'date' : Utils.get_date(), 'numSlouch' : 0},
+                {'date' : Utils.get_date(), 'numSlouch' : 0},
+                {'date' : Utils.get_date(), 'numSlouch' : 0},
+                {'date' : Utils.get_date(), 'numSlouch' : 0},
+                {'date' : Utils.get_date(), 'numSlouch' : 1}]}
+        #TODO: set data
+    else:
+        #UPDATE data value
+        data = users.val()
+        if data[6]['numSlouch'] != Utils.get_date():
+            #add new day to list
+            for i in range(0, 5):
+                data[i] = data[i+1]
+            data[6] = {'date' : Utils.get_date(), 'numSlouch' : 1}
+        else:
+            data[6]['numSlouch'] = data[6]['numSlouch'] + 1
+        #TODO: update data
 
     def json(self) -> Dict:
         return {
