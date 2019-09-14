1676e31df9db448f928338ed9487d1965417ad92
pmielnik <patricia.mielnik1@gmail.com>
2019-09-14 05:26:26 -0400

    accidentally put things in the wrong file...

--- views/users.py
+++ views/users.py
@@ -5,21 +5,6 @@ import datetime
 
 user_blueprint = Blueprint('users', __name__)
 
-firebaseConfig = {  #TODO
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com",
-  "serviceAccount": "path/to/serviceAccountCredentials.json" #optional; bypasses any security rules
-}
-
-firebase = pyrebase.initialize_app(firebaseConfig)
-
-# Get a reference to the firebaseAuth service
-firebaseAuth = firebase.firebaseAuth()
-# Get a reference to the database service
-firebaseDB = firebase.database()
-
 user = None # user variable???
 
 @user_blueprint.route('/register', methods=['GET', 'POST'])
@@ -29,13 +14,8 @@ def register_user():
         password = request.form['password']
         # registering the user
         try:
-            firebaseAuth.create_user_with_email_and_password(email, password)
-            global user = firebasefirebaseAuth.sign_in_with_email_and_password(email, password)
+            User.register_user(email, password)
             session['email'] = email
-
-            # create a path to data per user in DB 
-            firebaseDB.child("users").child.(email) 
-
             return redirect('/')
         except errors.UserError as e:
             flash(e.message, 'danger')
@@ -48,10 +28,9 @@ def login_user():
         email = request.form['email']
         password = request.form['password']
         try:
-            # Log the user in
-            global user = firebasefirebaseAuth.sign_in_with_email_and_password(email, password)
-            return redirect('/')
-            #TODO: every hour, need to run the following to get a new user token: user = firebasefirebaseAuth.refresh(user['refreshToken'])
+            if User.is_login_valid(email, password):
+                session['email'] = email
+                return redirect('/')
         except errors.UserError as e:
             flash(e.message, 'danger')
     return render_template('users/login.html')
@@ -61,31 +40,3 @@ def login_user():
 def logout_user():
     session['email'] = None
     return redirect('/')
-
-def user_slouch():
-    #TODO: send slouch notif command to FCM
-
-    #update times slouched list
-    data = firebaseDB.child('users').child(user['email']).get() #TODO: does the email field even exist in user?
-    if users.val() == None:
-        #SET data value
-        data = {'timesSlouched': [
-                        {'date' : datetime.date, 'numSlouch' : 0},
-                        {'date' : datetime.date, 'numSlouch' : 0},
-                        {'date' : datetime.date, 'numSlouch' : 0},
-                        {'date' : datetime.date, 'numSlouch' : 0},
-                        {'date' : datetime.date, 'numSlouch' : 0},
-                        {'date' : datetime.date, 'numSlouch' : 0},
-                        {'date' : datetime.date, 'numSlouch' : 1}]}
-        firebaseDB.child('users').child(user['email']).set(data)
-    else:
-        #UPDATE data value
-        data = users.val()
-        if data[6]['numSlouch'] != datetime.date:
-            #add new day to list
-            for i in range(0, 5):
-                data[i] = data[i+1]
-            data[6] = {'date' : datetime.date, 'numSlouch' : 1}
-        else:
-            data[6]['numSlouch'] = data[6]['numSlouch'] + 1
-        firebaseDB.child('users').child(user['email']).update(data)
