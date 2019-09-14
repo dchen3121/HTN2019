e2dd429449fc4b82dfa1fcb7031e99f37bcee743
pmielnik <patricia.mielnik1@gmail.com>
2019-09-14 04:46:23 -0400

    minor changes for firebase impl

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, redirect, url_for, session, render_template
 # from views.some_stuff import ...
+import pyrebase
 
 app = Flask(__name__)
 app.secret_key = "s7@8ob3fn8$%9nf64o&bv02q84gn74v!o78o34u78#27g4fno49lu*7h87q23fb1ui"
@@ -11,7 +12,6 @@ app.config.update(
 # we might not need this, change later
 
 # app.register_blueprint(alert_blueprint, url_prefix="/...")  # do this later
-
 
 @app.route('/')
 def home():
