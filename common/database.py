2fd6e3ccb2e0065f7078699d0d129c56e9ee77cf
dchen3121 <dchen001213@gmail.com>
2019-09-14 02:10:52 -0400

    Added user, need to change db from mongo to firebase

---
+++ common/database.py
@@ -0,0 +1,38 @@
+from typing import Dict
+import pymongo
+import os
+
+
+class Database:
+    # For localhost testing
+    # URI = "mongodb://127.0.0.1:27017/pricing"
+    # DATABASE = pymongo.MongoClient(URI).get_database()
+
+    URI = os.environ.get('MONGODB_URI')
+    DATABASE = pymongo.MongoClient(URI).get_default_database()
+
+    @staticmethod
+    def insert(collection: str, data: Dict):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection: str, query: Dict) -> pymongo.cursor:  # cursor kind of behaves like a list
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection: str, query: Dict) -> Dict:
+        return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def update(collection: str, query: Dict, data: Dict):
+        Database.DATABASE[collection].update(query, data, upsert=True)
+
+    @staticmethod
+    def remove(collection: str, query: Dict):
+        Database.DATABASE[collection].remove(query)
+
+    @staticmethod
+    def find_all_sorted_by(collection: str, query: Dict, key: str, ascending: bool):
+        if ascending:
+            return Database.DATABASE[collection].find(query).sort(key, pymongo.ASCENDING)
+        return Database.DATABASE[collection].find(query).sort(key, pymongo.DESCENDING)
---
+++ views/users.py
@@ -0,0 +1,40 @@
+from flask import request, session, render_template, Blueprint, redirect, url_for, flash
+from models.user import User, errors, requires_login
+from common.utils import Utils
+
+user_blueprint = Blueprint('users', __name__)
+
+
+@user_blueprint.route('/register', methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # registering the user
+        try:
+            User.register_user(email, password)
+            session['email'] = email
+            return redirect('/')
+        except errors.UserError as e:
+            flash(e.message, 'danger')
+    return render_template('users/register.html')
+
+
+@user_blueprint.route('/login', methods=['GET', 'POST'])
+def login_user():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            if User.is_login_valid(email, password):
+                session['email'] = email
+                return redirect('/')
+        except errors.UserError as e:
+            flash(e.message, 'danger')
+    return render_template('users/login.html')
+
+
+@user_blueprint.route('/logout')
+def logout_user():
+    session['email'] = None
+    return redirect('/')
