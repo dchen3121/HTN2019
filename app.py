80ff501ad6564747f0c7dc3fab7eccf52ec2f0a6
dchen3121 <dchen001213@gmail.com>
2019-09-14 05:34:38 -0400

    haha revert :'(

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, redirect, url_for, session, render_template
 # from views.some_stuff import ...
-import pyrebase
 
 app = Flask(__name__)
 app.secret_key = "s7@8ob3fn8$%9nf64o&bv02q84gn74v!o78o34u78#27g4fno49lu*7h87q23fb1ui"
--- views/users.py
+++ views/users.py
@@ -1,11 +1,9 @@
 from flask import request, session, render_template, Blueprint, redirect, url_for, flash
 from models.user import User, errors, requires_login
 from common.utils import Utils
-import datetime
 
 user_blueprint = Blueprint('users', __name__)
 
-user = None # user variable???
 
 @user_blueprint.route('/register', methods=['GET', 'POST'])
 def register_user():
@@ -40,3 +38,42 @@ def login_user():
 def logout_user():
     session['email'] = None
     return redirect('/')
+
+
+@user_blueprint.route('/settings')
+@requires_login
+def settings():
+    return render_template('users/settings.html')
+
+
+@user_blueprint.route('/change_password', methods=['POST'])
+def change_password():
+    if request.method == 'POST':
+        current_password = request.form['current']
+        user = User.find_by_email(session['email'])
+        current_password_confirm = user.password
+        new_password = request.form['new-password']
+        new_password_confirm = request.form['new-password-confirm']
+        if not Utils.check_hashed_password(current_password, current_password_confirm):
+            flash('Incorrect password.', 'danger')
+        elif new_password != new_password_confirm:
+            flash('The passwords entered do not match.', 'danger')
+        else:
+            user.password = Utils.hash_password(new_password)
+            user.save_to_mongo()
+    return redirect(url_for('users.settings'))
+
+
+@user_blueprint.route('/change_email', methods=['POST'])
+def change_email():
+    if request.method == 'POST':
+        new_email = request.form['new-email']
+        new_email_confirm = request.form['new-email-confirm']
+        if new_email != new_email_confirm:
+            flash('The emails entered do not match.', 'danger')
+        else:
+            user = User.find_by_email(session['email'])
+            user.email = new_email
+            session['email'] = new_email
+            user.save_to_mongo()
+    return redirect(url_for('users.settings'))
