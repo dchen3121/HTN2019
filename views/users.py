b908ec13480886c44b90ebf20044e81f36b8b4ea
pmielnik <patricia.mielnik1@gmail.com>
2019-09-14 05:07:31 -0400

    add db updates when slouch noted

--- views/users.py
+++ views/users.py
@@ -1,6 +1,7 @@
 from flask import request, session, render_template, Blueprint, redirect, url_for, flash
 from models.user import User, errors, requires_login
 from common.utils import Utils
+import datetime
 
 user_blueprint = Blueprint('users', __name__)
 
@@ -63,9 +64,28 @@ def logout_user():
 
 def user_slouch():
     #TODO: send slouch notif command to FCM
-    #update times slouched list?
-    data = db.child("users").child(user['email']).get() #TODO: uh does the email field even exist in user?
+
+    #update times slouched list
+    data = firebaseDB.child('users').child(user['email']).get() #TODO: uh does the email field even exist in user?
     if users.val() == None:
         #SET data value
+        data = {'timesSlouched': [
+                        {'date' : datetime.date, 'numSlouch' : 0},
+                        {'date' : datetime.date, 'numSlouch' : 0},
+                        {'date' : datetime.date, 'numSlouch' : 0},
+                        {'date' : datetime.date, 'numSlouch' : 0},
+                        {'date' : datetime.date, 'numSlouch' : 0},
+                        {'date' : datetime.date, 'numSlouch' : 0},
+                        {'date' : datetime.date, 'numSlouch' : 1}]}
+        firebaseDB.child('users').child(user['email']).set(data)
     else:
         #UPDATE data value
+        data = users.val()
+        if data[6]['numSlouch'] != datetime.date:
+            #add new day to list
+            for i in range(0, 5):
+                data[i] = data[i+1]
+            data[6] = {'date' : datetime.date, 'numSlouch' : 1}
+        else:
+            data[6]['numSlouch'] = data[6]['numSlouch'] + 1
+        firebaseDB.child('users').child(user['email']).update(data)
