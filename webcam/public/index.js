d378c500829ca5913c5af74ead3acddd93f567e5
dchen3121 <dchen001213@gmail.com>
2019-09-14 23:31:23 -0400

    fixed static importing

--- webcam/public/index.js
+++ webcam/public/index.js
@@ -29,7 +29,8 @@ if (navigator.mediaDevices.getUserMedia) {
         data = JSON.parse(message.data);
         console.log(data);
         slouchRoot.textContent = data.predictions[0].tagName;
-        //TODO: connect to py backend at {url}/users/update to update # slouch
+        request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+        //TODO: connect to py backend at localhost:4999/users/update to update # slouch
 
     }
 }
